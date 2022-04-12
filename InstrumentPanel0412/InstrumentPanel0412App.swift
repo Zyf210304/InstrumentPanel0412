d7da10825c8481c5891589016b9bad1fca04953f
张亚飞 <672233580@qq.com>
2022-04-12 13:50:25 +0800

    Initial Commit

---
+++ InstrumentPanel0412/InstrumentPanel0412App.swift
@@ -0,0 +1,17 @@
+//
+//  InstrumentPanel0412App.swift
+//  InstrumentPanel0412
+//
+//  Created by 张亚飞 on 2022/4/12.
+//
+
+import SwiftUI
+
+@main
+struct InstrumentPanel0412App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
